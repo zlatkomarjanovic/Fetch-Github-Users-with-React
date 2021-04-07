92772fba808cc762d77f841fa328c593a1f79db4
Zlatko Marjanovic <zlajaa2000@gmail.com>
2021-04-07 21:52:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment, Component} from 'react';
+import React, {Fragment, useState} from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './Components/layout/Navbar';
@@ -9,14 +9,14 @@ import Alert from './Components/layout/Alert';
 import axios from 'axios';
 import About from './Components/pages/About';
 
-class App extends Component {
-  state = {
-    users: [],
-    loading: false,
-    alert:null,
-    user : {},
-    repos: []
-  }
+const App =() =>{
+  const [users, setUsers] = useState([]);
+  const [user, setUsers] = useState({});
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(null);
+
+
 
 
 //  async componentDidMount() {
@@ -33,44 +33,50 @@ class App extends Component {
 
 //Search Github users
   searchUsers = async text =>{
-  this.setState({loading : true});
+  setLoading(true);
 
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
       {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-    this.setState({users: res.data.items, loading: false});
+    setUsers(res.data.items);
+    setLoading(false);
   }
 //Get single Github USER
 
 getUser = async (username) => {
-  this.setState({loading : true});
+  setLoading(true);
 
     const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
       {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-    this.setState({repos: res.data, loading: false});
+        setUser(res.data);
+    setLoading(false);
 }
 //Get users Repos
 getUserRepos = async username => {
-  this.setState({loading : true});
+  setLoading(true);
 
     const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
       {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-    this.setState({user: res.data, loading: false});
+    setRepos(res.data);
+    setLoading(false);
 }
 
 
 //Clear users from setState
 
-clearUsers = () => this.setState({users:[], loading: false});
+clearUsers = () =>  {
+  setUsers([]);
+  setLoading(false);
+}
 
 
 //Set setAlert
 setAlert = (msg, type) => {
-  this.setState({ alert: {msg, type} });
 
-  setTimeout(() => this.setState({ alert: null}), 5000);
+  set.Alert({msg, type});
+  setTimeout(() => setAlert(null), 5000);
 }
 
 
